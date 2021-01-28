289b3e343b958eff180ce5b255ea4569da5b2a62
Akndju <davidd.akindoju@gmail.com>
2021-01-28 14:29:57 +0100

    Created Header Component

---
+++ src/Components/header/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Simple React Landing Page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </div>
+  );
+};
+
+export default Header;
